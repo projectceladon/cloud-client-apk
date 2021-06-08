195e555ce92e202436ab688000014f33234f3fa7
yingzhex <yingzhenx.li@intel.com>
2021-06-08 10:38:51 +0800

    Check if the device is rooted
    Signed-off-by: yingzhex <yingzhenx.li@intel.com>

--- aic-application/app/src/androidTest/java/com/intel/gamepad/ExampleInstrumentedTest.kt
+++ aic-application/app/src/androidTest/java/com/intel/gamepad/ExampleInstrumentedTest.kt
@@ -1,12 +1,14 @@
 package com.intel.gamepad
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,8 +19,49 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
+        assertTrue("The device is rooted", isDeviceRooted())
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.intel.gamepad", appContext.packageName)
+    }
+    fun isDeviceRooted(): Boolean {
+        return checkRootMethodOne() || checkRootMethodTwo() || checkRootMethodThree()
+    }
+
+    private fun checkRootMethodOne(): Boolean {
+        val buildTags = Build.TAGS
+        return buildTags != null && buildTags.contains("test-keys")
+    }
+
+    private fun checkRootMethodTwo(): Boolean {
+        val paths = arrayOf(
+            "/system/app/Superuser.apk",
+            "/sbin/su",
+            "/system/bin/su",
+            "/system/xbin/su",
+            "/data/local/xbin/su",
+            "/data/local/bin/su",
+            "/system/sd/xbin/su",
+            "/system/bin/failsafe/su",
+            "/data/local/su",
+            "/su/bin/su"
+        )
+        for (path in paths) {
+            if (File(path).exists()) return true
+        }
+        return false
+    }
+
+    private fun checkRootMethodThree(): Boolean {
+        var process: Process? = null
+        return try {
+            process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
+            val `in` = BufferedReader(InputStreamReader(process.inputStream))
+            if (`in`.readLine() != null) true else false
+        } catch (t: Throwable) {
+            false
+        } finally {
+            process?.destroy()
+        }
     }
 }
