aec939bde4e13552ed2e7ffd9e35122df221a5da
yingzhex <yingzhenx.li@intel.com>
2021-06-10 10:27:49 +0800

    Check if the device is rooted in splash activity
    Signed-off-by: yingzhex <yingzhenx.li@intel.com>

--- aic-application/app/src/androidTest/java/com/intel/gamepad/ExampleInstrumentedTest.kt
+++ aic-application/app/src/androidTest/java/com/intel/gamepad/ExampleInstrumentedTest.kt
@@ -19,49 +19,8 @@ import java.io.InputStreamReader
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        assertTrue("The device is rooted", isDeviceRooted())
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.intel.gamepad", appContext.packageName)
-    }
-    fun isDeviceRooted(): Boolean {
-        return checkRootMethodOne() || checkRootMethodTwo() || checkRootMethodThree()
-    }
-
-    private fun checkRootMethodOne(): Boolean {
-        val buildTags = Build.TAGS
-        return buildTags != null && buildTags.contains("test-keys")
-    }
-
-    private fun checkRootMethodTwo(): Boolean {
-        val paths = arrayOf(
-            "/system/app/Superuser.apk",
-            "/sbin/su",
-            "/system/bin/su",
-            "/system/xbin/su",
-            "/data/local/xbin/su",
-            "/data/local/bin/su",
-            "/system/sd/xbin/su",
-            "/system/bin/failsafe/su",
-            "/data/local/su",
-            "/su/bin/su"
-        )
-        for (path in paths) {
-            if (File(path).exists()) return true
-        }
-        return false
-    }
-
-    private fun checkRootMethodThree(): Boolean {
-        var process: Process? = null
-        return try {
-            process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
-            val `in` = BufferedReader(InputStreamReader(process.inputStream))
-            if (`in`.readLine() != null) true else false
-        } catch (t: Throwable) {
-            false
-        } finally {
-            process?.destroy()
-        }
     }
 }
