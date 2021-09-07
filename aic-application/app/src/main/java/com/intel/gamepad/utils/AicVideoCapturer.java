d701183806162c0b05db4b617bece2a94394a75e
Liu, Kai1 <kai1.liu@intel.com>
2021-09-07 13:04:47 +0800

    Add atrace points for camera latency calculating
    Signed-off-by: Liu, Kai1 <kai1.liu@intel.com>

--- aic-application/app/src/main/java/com/intel/gamepad/utils/AicVideoCapturer.java
+++ aic-application/app/src/main/java/com/intel/gamepad/utils/AicVideoCapturer.java
@@ -9,6 +9,10 @@ import org.webrtc.CameraEnumerator;
 import owt.base.Stream;
 import owt.base.VideoCapturer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public final class AicVideoCapturer extends Camera1Capturer implements VideoCapturer {
     private int width, height, fps;
 
@@ -18,7 +22,31 @@ public final class AicVideoCapturer extends Camera1Capturer implements VideoCapt
 
     public static AicVideoCapturer create(int width, int height) {
         String deviceName = getDeviceName(true);
-        AicVideoCapturer capturer = new AicVideoCapturer(deviceName, true);
+        AicVideoCapturer capturer = null;
+        String cameraLatencyDebugEnabled = null;
+        BufferedReader input = null;
+        try {
+            Process p = Runtime.getRuntime().exec("getprop camera.latency.debug");
+            input = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
+            cameraLatencyDebugEnabled = input.readLine();
+            input.close();
+        } catch (IOException ex) {
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (cameraLatencyDebugEnabled != null && cameraLatencyDebugEnabled.equals("true")) {
+            capturer = new AicVideoCapturer(deviceName, false);
+        } else {
+            capturer = new AicVideoCapturer(deviceName, true);
+        }
         capturer.width = width;
         capturer.height = height;
         capturer.fps = 30;
